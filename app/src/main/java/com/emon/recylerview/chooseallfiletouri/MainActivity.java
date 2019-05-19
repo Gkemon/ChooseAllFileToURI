461f438ea01970fb48deba55828bfae6f0e3f440
Emon <emon@field.buzz>
2019-05-19 12:02:37 +0600

    Initial commit

---
+++ app/src/main/java/com/emon/recylerview/chooseallfiletouri/MainActivity.java
@@ -0,0 +1,39 @@
+package com.emon.recylerview.chooseallfiletouri;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_upload).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //creating an intent for file chooser
+                Intent intent = new Intent();
+                intent.setType("*/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1){
+            if(data!=null) Log.d("GK","file uri"+data.getData());
+            else Log.d("GK","file not null");
+        }
+    }
+}
